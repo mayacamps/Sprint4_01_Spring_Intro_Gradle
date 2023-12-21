ca60b7317714409778abd2def996ab35be55ffcd
Maya <maya.camps@estudiantat.upc.edu>
2023-12-21 11:38:24 +0100

    feature salute

--- src/main/java/cat/itacademy/barcelonactiva/camps/maya/s04/t01/n02/controllers/HelloWorldController.java
+++ src/main/java/cat/itacademy/barcelonactiva/camps/maya/s04/t01/n02/controllers/HelloWorldController.java
@@ -1,4 +1,13 @@
 package cat.itacademy.barcelonactiva.camps.maya.s04.t01.n02.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+    @GetMapping("/HelloWorld")
+    public String salute(@RequestParam(value = "name", defaultValue = "UNKNOWN") String name){
+        return "Hi, " + name + ". You're running a Gradle project.";
+    }
 }
